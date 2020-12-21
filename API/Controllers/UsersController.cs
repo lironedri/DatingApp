a6489b1603c1be7e719ecc4f4cb36944145b6ded
LironA.Cybrtx <LironA.Cybrtx@gmail.com>
2020-12-21 13:29:08 +0200

    End of UsersController (basic)

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // returning list of users asynchronously. When a request go the DB, this code pauses,
+        // it waits. It deferred to a task that then goes and makes the quary to the DB.     
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            // when the task comes back - I get the result out of the task by using the awaits
+            return await _context.Users.ToListAsync();
+        }
+
+        // api/users/3
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
